88e79952b16a6996bf1744d0ddaafd809763994a
Jordan Roberts <jrobertswebdev@gmail.com>
2022-08-17 20:59:15 -0500

    Header functional

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-
+import styles from './Header.module.css'
 import Logo from './Logo'
 import { Twirl as Hamburger } from 'hamburger-react'
 
@@ -31,139 +31,75 @@ export default function Header() {
   }, [])
 
   return (
-    <div className='container'>
-      <header className={`mainHeader ${scrollPosition > 199 && direction === 'down' ? 'hide' : 'show'} `}>
-        <Link href='/'>
-          <div className='logo'>
-            <Logo />
+    <header
+      className={`${styles.mainHeader} header ${scrollPosition > 199 && direction === 'down' ? 'hide' : 'show'} `}>
+      <div className={`headerBg ${showBg && 'isOpen'} `}></div>
+      <Link href='/'>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+      </Link>
+      <nav className={styles.navBar}>
+        <ul className={styles.navLeft}>
+          <li>
+            <Link href='/vehicles/falcon-9'>FALCON 9</Link>
+          </li>
+          <li>
+            <Link href='/vehicles/falcon-heavy'>FALCON HEAVY</Link>
+          </li>
+          <li>
+            <Link href='/vehicles/dragon'>DRAGON</Link>
+          </li>
+          <li>
+            <Link href='/vehicles/starship'>STARSHIP</Link>
+          </li>
+          <li>
+            <Link href='/vehicles/human-spaceflight'>HUMAN SPACEFLIGHT</Link>
+          </li>
+          <li>
+            <Link href='/vehicles/rideshare'>RIDESHARE</Link>
+          </li>
+          <li>
+            <Link href='www.starlink.com'>STARLINK</Link>
+          </li>
+        </ul>
+        <ul className={styles.navRight}>
+          <li>
+            <Link href='shop.spacex.com'>SHOP</Link>
+          </li>
+          <div className={styles.hamburger}>
+            <Hamburger size={16} direction='left' label='Show menu' />
           </div>
-        </Link>
-        <nav className='navBar'>
-          <ul className='navLeft'>
-            <li>
-              <Link href='/vehicles/falcon-9'>FALCON 9</Link>
-            </li>
-            <li>
-              <Link href='/vehicles/falcon-heavy'>FALCON HEAVY</Link>
-            </li>
-            <li>
-              <Link href='/vehicles/dragon'>DRAGON</Link>
-            </li>
-            <li>
-              <Link href='/vehicles/starship'>STARSHIP</Link>
-            </li>
-            <li>
-              <Link href='/vehicles/human-spaceflight'>HUMAN SPACEFLIGHT</Link>
-            </li>
-            <li>
-              <Link href='/vehicles/rideshare'>RIDESHARE</Link>
-            </li>
-            <li>
-              <Link href='www.starlink.com'>STARLINK</Link>
-            </li>
-          </ul>
-          <ul className='navRight'>
-            <li>
-              <Link href='shop.spacex.com'>SHOP</Link>
-            </li>
-            <div className='hamburger'>
-              <Hamburger size={16} direction='left' label='Show menu' />
-            </div>
-          </ul>
-        </nav>
-        <style jsx>{`
-          .logo {
-            position: absolute;
-            width: 200px;
-            height: 27px;
-            left: 25px;
-            top: 0.75rem;
-            fill: #fff;
-          }
-          .container {
-            position: fixed;
-          }
-
-          .mainHeader ul {
-            display: flex;
-            align-items: flex-end;
-          }
-
-          .mainHeader li {
-            font-size: var(--fs-small);
-            font-weight: 400;
-            margin-inline: 12px;
-            position: relative;
-          }
-
-          .navBar {
-            width: 100%;
-            display: flex;
-            justify-content: space-between;
-            margin-left: 290px;
-            margin-block: 2rem;
-          }
-
-          .navRight {
-            position: relative;
-            width: 8rem;
-          }
-
-          .hamburger {
-            position: absolute;
-            left: 4rem;
-            bottom: -14px;
-          }
-
-          /* Menu Items Animation */
-          .navBar a::after {
-            content: '';
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            height: 1px;
-            background-color: #fff;
-            transform: scaleX(0);
-            transition: transform 0.6s cubic-bezier(0.19, 1, 0.22, 1);
-            transform-origin: right center;
-          }
-
-          .navBar a:hover::after {
-            transform: scaleX(1);
-            transform-origin: left center;
-            transition-duration: 0.4s;
-          }
-          .mainHeader {
-            display: flex;
-            width: 100%;
-            max-width: 1400px;
-            margin: 0 auto;
-            position: fixed;
-            transition: opacity 0.3s ease;
-          }
-          .hide {
-            opacity: 0;
-            pointer-events: none;
-            transition: opacity 0.3s ease;
-          }
-          .show {
-            opacity: 1;
-          }
-          .mainHeader::before {
-            content: '';
-            position: absolute;
-            top: -85px;
-            left: 0;
-            background-color: #000;
-            width: 100%;
-            height: 100%;
-            top: ${showBg ? 0 : '-85px'};
-            transition: top 0.4s ease-in-out;
-          }
-        `}</style>
-      </header>
-    </div>
+        </ul>
+      </nav>
+      <style jsx>{`
+        .header {
+          transition: opacity 0.3s ease;
+        }
+        .hide {
+          opacity: 0;
+          pointer-events: none;
+          transition: opacity 0.2s ease;
+        }
+        .show {
+          opacity: 1;
+        }
+        .headerBg {
+          position: absolute;
+          top: 0;
+          left: 0;
+          background-color: #000;
+          width: 100%;
+          height: 100%;
+          transform: translateY(-100%);
+          transition: transform 0.7s cubic-bezier(0.19, 1, 0.22, 1) 0.15s;
+        }
+        .isOpen {
+          transform: none;
+          transition-delay: 0s;
+        }
+      `}</style>
+    </header>
   )
 }
 
